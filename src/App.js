ef29c963c1b319372fcf5cbcec9e1cb64dea3ae8
Noah-1711 <noahnirmal11@gmail.com>
2023-02-27 00:44:43 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import { CredExperience } from './components/CredExperience/CredExperience';
 import { Credsecurity } from './components/Credsecurity/Credsecurity';
 import { Credstory } from './components/Credstory/Credstory';
 import { Feelspecial } from './components/Fellspecial/Feelspecial';
+import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Headsection } from './components/Headingsection/Headsection';
 import { Imagesection } from './components/Imagesection/Imagesection';
 import { Rating } from './components/Rating/Rating';
 import {  Windowpeak } from './components/Windowpeak/Windowpeak';
+
+
 
 
 function App() {
@@ -24,6 +27,7 @@ function App() {
       <Credsecurity />
       <Credstory />
       <Rating />
+      <Footer />
      
     </div>
   );
